cf1c7b833f673021c43e3fa1429a9d79f659868f
root <root@localhost.localdomain>
2017-01-28 15:35:06 +0000

    third tag

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.handler = (event, context, callback) => {
-    console.log("start handler version2.0");
+    console.log("start handler version3.0");
     console.log("NODE_ENV is " + process.env.NODE_ENV);
     console.log("end handler");
 };
